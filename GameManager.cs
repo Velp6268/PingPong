a196d2c74603a6fa3244d9badba264ac295205a0
Velp6268 <90183727+Velp6268@users.noreply.github.com>
2023-02-05 15:05:17 +0300

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Pad pad;
+
+    public Ball ball;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 bottomRight;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
